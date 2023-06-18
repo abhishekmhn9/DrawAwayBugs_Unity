145a83fb30ad8f0d2f71b296eaed3948b81a12b5
abhishekmhn9 <abhishekmhn9@hotmail.com>
2023-06-19 01:53:12 +0530

    Animation-added

--- Assets/Scripts/LineDraw.cs
+++ Assets/Scripts/LineDraw.cs
@@ -10,19 +10,19 @@ public class LineDraw : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.Positions = new Vector2[] { new Vector2(0, 0) };
+        //lineRenderer.transform.position = new Vector3[] { new Vector3(0, 0, 0) };
     }
 
     void Update()
     {
-        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        /*if (Input.GetTouch(0).phase == TouchPhase.Began)
         {
             touchPosition = Input.GetTouch(0).position;
         }
         else if (Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-            lineRenderer.Positions.Add(touchPosition);
-        }
+            lineRenderer.transform.position.Add(touchPosition);
+        }*/
     }
 
 
